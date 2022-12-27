514c622767603fcd6b4d6c5d4556845e2301c81d
Alejandro R <alejandro.reyesj@outlook.com>
2022-12-27 18:51:21 -0500

    Removed format display.

--- src/lib.rs
+++ src/lib.rs
@@ -88,16 +88,3 @@ impl Universe {
         self.cells = next;
     }
 }
-
-// impl fmt::Display for Universe {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         for line in self.cells.as_slice().chunks(self.width as usize) {
-//             for &cell in line {
-//                 let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-//                 write!(f, "{symbol}")?;
-//             }
-//             writeln!(f)?;
-//         }
-//         Ok(())
-//     }
-// }
